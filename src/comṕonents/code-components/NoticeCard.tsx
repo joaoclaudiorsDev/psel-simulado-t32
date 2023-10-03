52f24d437b38ef09984e6f558d3257f0bad4fe2a
João Cláudio <joaoclaudiobio@gmail.com>
2023-10-03 16:10:42 -0300

    começando o grid

--- src/comṕonents/code-components/NoticeCard.tsx
+++ src/comṕonents/code-components/NoticeCard.tsx
@@ -5,6 +5,7 @@ import LerMais from "./LerMais";
 import NoticeButton from "./NoticeButton";
 import { FavoritesContext } from "./FavoriteContext";
 import FavoriteImageButton from "../../utils/FavoriteButton";
+import '../css-components/NoticeCard.css'
 
 function NoticeCard() {
   const [noticias, setNotices] = useState([]);
@@ -39,8 +40,15 @@ function NoticeCard() {
     <>
       <div className="wrapper-container-notice">
         {noticias.map((noticia) => (
-          <div className="notice-list" key={noticia.id}>
-           <FavoriteImageButton
+          <div>
+            <div className="notice-list" key={noticia.id}>
+              <h3>{noticia.titulo}</h3>
+              <p>{noticia.introducao}</p>
+              <p>{calculateDaysAgo(noticia.data_publicacao)}</p>
+              <LerMais link={noticia.link} />
+            </div>
+            <div>
+              <FavoriteImageButton
               id={noticia.id}
               isFavorite={isFavorite(noticia.id)}
               toggleFavorite={toggleFavorite}
@@ -49,10 +57,7 @@ function NoticeCard() {
               data_publicacao={noticia.data_publicacao}
               link={noticia.link}
             />
-            <h3>{noticia.titulo}</h3>
-            <p>{noticia.introducao}</p>
-            <p>{calculateDaysAgo(noticia.data_publicacao)}</p>
-            <LerMais link={noticia.link} />
+            </div>
           </div>
         ))}
       </div>
